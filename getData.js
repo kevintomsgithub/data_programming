79b429ab6ecb0084fb6cbb39909aa44b08213f5b
kartiez <43430553+kartiez@users.noreply.github.com>
2019-11-29 22:15:47 -0500

    Delete getData.js

--- getData.js
+++
@@ -1,6 +0,0 @@
-var DBConn = require(__dirname + '\\Connection.js')
-var sqlquery = "SELECT TOP (1000) * FROM [Playstore].[dbo].[Data] "
-DBConn.execSql(sqlquery, function (res) {
-
-console.log(res);
-  });
